302f6e84cb3cec28ca9080066b574025640055de
Temitope Ayotunde <brhamix@gmail.com>
2026-03-05 00:05:53 +0100

    workings: register a new tool

--- src/index.ts
+++ src/index.ts
@@ -15,8 +15,8 @@ server.registerTool(
     title: "Generates a fake person data using naija-faker tool",
     description: "Generates a fake person data. Accepts a string payload for the language of the person data and a string payload for the gender of the person data. The available languages are Hausa, Igbo and Yoruba. The accepted gender values are male and female. Returns the processed result as an object",
     inputSchema: {
-      language: z.string().describe("The language of the person data. The available languages are Hausa, Igbo and Yoruba"),
-      gender: z.string().describe("The gender of the person data. The accepted gender values are male and female"),
+      language: z.string().optional().describe("The language of the person data. The available languages are Hausa, Igbo and Yoruba"),
+      gender: z.string().optional().describe("The gender of the person data. The accepted gender values are male and female"),
     }
   },
   async ({ language, gender }) => {
@@ -24,6 +24,30 @@ server.registerTool(
       const person = faker.person(language as "yoruba" | "igbo" | "hausa", gender as "male" | "female");
       return {
         content: [{ type: "text", text: JSON.stringify(person, null, 2) }],
+      };
+    } catch (error) {
+      return {
+        content: [{ type: "text", text: `Error: ${error}` }],
+        isError: true,
+      };
+    }
+  }
+);
+
+server.registerTool(
+  "generate_fake_people",
+  {
+    title: "Generate a list of fake people using naija-faker tool",
+    description: "Generates a list of fake people data. Accepts a number payload the number of persons to be generated as part of the people list. Returns the processed results as an array of object",
+    inputSchema: {
+      count: z.number().optional().describe("The number of persons to be generated as part of the people list")
+    }
+  },
+  async ({ count }) => {
+    try {
+      const people = faker.people(count);
+      return {
+        content: [{ type: "text", text: JSON.stringify(people, null, 2) }],
       };
     } catch (error) {
       return {
