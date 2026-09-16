0a6e0426e8911fde2ce7fe9a054fcb5eff75694a
Temitope Ayotunde <brhamix@gmail.com>
2026-09-16 16:56:15 +0100

    workings: update version to 2.0.0 in package.json and package-lock.json

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import { registerRecordsTools } from "./tools/records-registration.js";
 
 const server = new McpServer({
   name: "Naija Faker Library",
-  version: "1.0.1",
+  version: "2.0.0",
 });
 
 registerPersonTools(server);
